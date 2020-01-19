85333acc244548f22ed0417207cf02f9fc58a60f
Danny Allegrezza <danny@dannyallegrezza.com>
2020-01-19 12:33:06 -0500

    Close out issue #1 - validate VSCode Setup

--- CatFeeder.ino
+++ CatFeeder.ino
@@ -1,9 +1,10 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 200;                    // change this to fit the number of steps per revolution
 Stepper myStepper(stepsPerRevolution, D1, D2, D5, D6); // initialize the stepper motor on preferred pins of ESP8266
 
-void setup() {
+void setup()
+{
 
   // set the speed at 100 rpm:
   myStepper.setSpeed(100);
@@ -11,18 +12,19 @@ void setup() {
   feed();
 }
 
-void loop() {
-
+void loop()
+{
 }
 
-void feed() {
+void feed()
+{
   Serial.println("feeding!");
   // danny: for debugging purposes, was just verifying that I could use the const values for the digitalWrite calls below..
   Serial.println(D1);
   Serial.println(D2);
   Serial.println(D5);
   Serial.println(D6);
-  
+
   // step one revolution - in one direction:
   myStepper.step(stepsPerRevolution);
 
@@ -30,6 +32,6 @@ void feed() {
   digitalWrite(D2, LOW);
   digitalWrite(D5, LOW);
   digitalWrite(D6, LOW);
-  
+
   delay(5000);
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # meow-mix
 An API driven dashboard for my ESP8266 based automatic pet feeder project.
+
+## ESP8266 VSCode Config
+The free Arduino is great, however, if you prefer to use VSCode you can install the Arduino extension. When the extension finds your `.ino` file it will allow you to configure your board type as well as the programmer and serial port. 
+
+> NOTE: On Windows 10 I needed to add the ESP8266 "Board Package" in order for this to work.
+
+The generated `.vscode/arduino.json` file will look something like this:
+
+```json
+{
+    "port": "COM6",
+    "board": "esp8266:esp8266:nodemcuv2",
+    "configuration": "xtal=80,vt=flash,exception=legacy,ssl=all,eesz=4M2M,led=2,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200",
+    "programmer": "AVRISP mkII",
+    "sketch": "CatFeeder.ino",
+    "output": "../build"
+}
+```
