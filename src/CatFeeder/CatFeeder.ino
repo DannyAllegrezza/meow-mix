7f9a41cf0ece43f63998af46fa1f1bfde4e34503
Danny Allegrezza <danny@dannyallegrezza.com>
2020-07-17 23:47:38 -0400

    Adjust speed to allow for more consistent turn

--- src/CatFeeder/CatFeeder.ino
+++ src/CatFeeder/CatFeeder.ino
@@ -30,7 +30,7 @@ void setup()
   pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
 
-  myStepper.setSpeed(100);
+  myStepper.setSpeed(55);
 
   setupWifi(config);
 
